1fd0e6d61cbdea32ac8645510ab49a546fcbabd2
Tomasz Cieśla <ciesla.tomasz.92@gmail.com>
2022-08-24 12:57:14 +0200

    cleaned up second fragment which became shopping list item add fragment

--- app/src/main/java/org/example/tciesla/shoppinglist/ShoppingListItemAddFragment.kt
+++ app/src/main/java/org/example/tciesla/shoppinglist/ShoppingListItemAddFragment.kt
@@ -1,39 +1,32 @@
 package org.example.tciesla.shoppinglist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import org.example.tciesla.shoppinglist.databinding.FragmentSecondBinding
+import org.example.tciesla.shoppinglist.databinding.FragmentShoppingListItemAddBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class ShoppingListItemAddFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentShoppingListItemAddBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentShoppingListItemAddBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().navigate(R.id.action_ShoppingListItemAddFragment_to_ShoppingListFragment)
         }
     }
 
